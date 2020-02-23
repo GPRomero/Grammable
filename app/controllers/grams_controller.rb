c5cf03637318b16e833decd12c7b574596249bf8
gpromero <gpromero221@gmail.com>
2020-02-23 02:02:11 +0000

    added tests for editing and updating

--- app/controllers/grams_controller.rb
+++ app/controllers/grams_controller.rb
@@ -4,14 +4,34 @@ class GramsController < ApplicationController
   def index
   end
 
+  def update
+    @gram = Gram.find_by_id(params[:id])
+    return render_not_found if @gram.blank?
+
+    @gram.update_attributes(gram_params)
+
+    if @gram.valid?
+      redirect_to root_path
+    else
+      return render :edit, status: :unprocessable_entity
+    end
+  end
+
   def new
     @gram = Gram.new
+  end
+
+  def edit
+    @gram = Gram.find_by_id(params[:id])
+    if @gram.blank?
+      return render_not_found if @gram.blank?
+    end
   end
 
   def show
     @gram = Gram.find_by_id(params[:id])
     if @gram.blank?
-      render plain: 'Not Found :(', status: :not_found
+      return render_not_found if @gram.blank?
     end
   end
 
@@ -28,6 +48,10 @@ class GramsController < ApplicationController
 
   def gram_params
     params.require(:gram).permit(:message)
+  end
+
+  def render_not_found
+    render plain: 'Not Found :(', status: :not_found
   end
   
 end
